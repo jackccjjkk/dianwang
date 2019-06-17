aaeed985000cf8dd9d67b5dd312f359dc84198f5
cjk <cjk@dlyt.net>
2019-06-18 07:45:42 +0800

    aaa

--- mapDemo/common.js
+++ mapDemo/common.js
@@ -1,46 +1,54 @@
-var  SERVER_PATH = "data/";
+var SERVER_PATH = "data/";
 
 function ajaxPost(url, param, success, error, showProgress) {
-  if (typeof (showProgress) == 'undefined' || showProgress) {
-    showLoading();
-  }
-  jQuery.ajax({
-    url: SERVER_PATH  + url,
-    type: 'POST',
-    timeout: 60000, // 超时时间设置，单位毫秒
-    data: JSON.stringify(param),
-    dataType: 'json',
-    contentType: 'application/json;charset=UTF-8',
-    success: eval(function (data) {
-      if (typeof (showProgress) == 'undefined' || showProgress) {
-        hideLoading();
-      }
-      success(data);
-    }),
-    error: eval(function (data) {
-      if (typeof (showProgress) == 'undefined' || showProgress) {
-        hideLoading();
-      }
-      if (typeof (shadeFlag) == 'undefined' || shadeFlag) {
-        // TODO 隐藏进度条
-      }
-      if (data.responseText == "login") {
-        showAlert("登录过期，请重新登录。", "提示", function () {
-          window.location.href = "login.html";
+    if (typeof (showProgress) == 'undefined' || showProgress) {
+        showLoading();
+    }
+    jQuery.ajax({
+        url: SERVER_PATH + url,
+        type: 'POST',
+        timeout: 60000, // 超时时间设置，单位毫秒
+        data: JSON.stringify(param),
+        dataType: 'json',
+        contentType: 'application/json;charset=UTF-8',
+        success: eval(function (data) {
+            if (typeof (showProgress) == 'undefined' || showProgress) {
+                hideLoading();
+            }
+            success(data);
+        }),
+        error: eval(function (data) {
+            if (typeof (showProgress) == 'undefined' || showProgress) {
+                hideLoading();
+            }
+            if (typeof (shadeFlag) == 'undefined' || shadeFlag) {
+                // TODO 隐藏进度条
+            }
+            if (data.responseText == "login") {
+                showAlert("登录过期，请重新登录。", "提示", function () {
+                    window.location.href = "login.html";
+                })
+            }
+            error(data);
         })
-      }
-      error(data);
-    })
-  });
+    });
 }
 
 function showLoading() {
-  //$("#subLoading").show();
-  $("body").mLoading("show");
+    //$("#subLoading").show();
+    $("body").mLoading("show");
 }
 
 function hideLoading() {
-  //$("#subLoading").hide();
-  $("body").mLoading("hide");
+    //$("#subLoading").hide();
+    $("body").mLoading("hide");
+}
+
+function showBottomPopup() {
+    $('.content-bottom').css('bottom', '0');
+}
+
+function hideBottomPopup() {
+    $('.content-bottom').css('bottom', '-200px');
 }
 
--- mapDemo/zdhd.js
+++ mapDemo/zdhd.js
@@ -1,3 +1,7 @@
+var kaiguanzhanList = null;
+var map = null;
+// 地图的中心，大连东港会议中心
+var centerPoint = new BMap.Point(121.685041, 38.927147);
 $(document).ready(function () {
     initMap();
     setLinstener();
@@ -6,7 +10,7 @@ $(document).ready(function () {
 });
 
 function initMap() {
-    var map = new BMap.Map("map", {mapType: BMAP_HYBRID_MAP})
+    map = new BMap.Map("map", {mapType: BMAP_HYBRID_MAP})
 //  var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
 //  var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
 //  var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，仅包含平移和缩放按钮
@@ -30,6 +34,7 @@ function getData() {
     var success = function (data) {
         if (data.resultCode = '00') {
             initProcessList(data.processList);
+            kaiguanzhanList = data.kaiguanzhanList;
         } else {
 
         }
@@ -47,10 +52,10 @@ function initProcessList(processList) {
         $(".process-item-date", $item).text(item.date);
         $(".process-item-desc", $item).text(item.desc);
         if (index == 0) {
-            $(".process-item-line-1", $item).css("background","transparent");
+            $(".process-item-line-1", $item).css("background", "transparent");
         }
         if (index == processList.length - 1) {
-            $(".process-item-line-2", $item).css("background","transparent");
+            $(".process-item-line-2", $item).css("background", "transparent");
         }
         $item.removeClass("template").appendTo("#processList");
     })
@@ -111,5 +116,69 @@ function setLinstener() {
     });
     $("#submenu-4").mouseout(function () {
         $("#submenu-4").hide();
+    });
+
+    $("#menu_zhansuo").click(function () {
+        // 清除地图上其他浮层，显示站所浮层
+        map.clearOverlays();
+        map.centerAndZoom(centerPoint, 15);
+        // 隐藏底部浮层
+        hideBottomPopup();
+        showKuaiguanzhanList();
+    })
+}
+
+/**
+ * 点击开关站菜单的时候在地图上显示开关站图标
+ */
+function showKuaiguanzhanList() {
+    $.each(kaiguanzhanList, function (index, item) {
+        var pt = new BMap.Point(item.longitude, item.latitude);
+        var myIcon = new BMap.Icon("img/map_icon_zs.png", new BMap.Size(66, 59));
+        var marker = new BMap.Marker(pt, {icon: myIcon});  // 创建标注
+        marker.addEventListener("click", function (type, target) {
+            showKuaiguanzhanDetail(item.id);
+        });
+        map.addOverlay(marker);
+    });
+}
+
+/**
+ * 点击地图的开关站图标，弹出下方浮层，显示开关站详情
+ */
+function showKuaiguanzhanDetail(id) {
+    showBottomPopup();
+    $.each(kaiguanzhanList, function (index, item) {
+        if (id == item.id) {
+            $("#kaiguanzhan_bdzmc").text(item.bdzmc);
+            $("#kaiguanzhan_sbmc").text(item.sbmc);
+            $("#kaiguanzhan_ssds").text(item.ssds);
+            $("#kaiguanzhan_ywdw").text(item.ywdw);
+            $("#kaiguanzhan_whbz").text(item.whbz);
+            $("#kaiguanzhan_zcxz").text(item.zcxz);
+            $("#kaiguanzhan_zcdw").text(item.zcdw);
+            $("#kaiguanzhan_dydj").text(item.dydj);
+            $("#kaiguanzhan_sbzt").text(item.sbzt);
+            $("#kaiguanzhan_tyrq").text(item.tyrq);
+            $("#kaiguanzhan_dzlx").text(item.dzlx);
+            $("#kaiguanzhan_gdqy").text(item.gdqy);
+            $("#kaiguanzhan_sfznbdz").text(item.sfznbdz);
+            $("#kaiguanzhan_sfsnz").text(item.sfsnz);
+            $("#kaiguanzhan_sfdw").text(item.sfdw);
+
+            $("#monitorDataList").html($("#monitorDataList .template.monitor-data-item"));
+            $.each(item.monitorDataList, function (index2, monitorData) {
+                var $monitorItem = $("#monitorDataList .template.monitor-data-item").clone();
+                $(".monitor-data-item-stationname",$monitorItem).text(monitorData.stationname);
+                $(".monitor-data-item-monitordevice",$monitorItem).text(monitorData.monitordevice);
+                $(".monitor-data-item-ambrtemp",$monitorItem).text(monitorData.ambrtemp);
+                $(".monitor-data-item-amp_pch",$monitorItem).text(monitorData.amp_pch);
+                $(".monitor-data-item-freq_pch",$monitorItem).text(monitorData.freq_pch);
+                $(".monitor-data-item-energy_pch",$monitorItem).text(monitorData.energy_pch);
+                $(".monitor-data-item-cabcttmp_a",$monitorItem).text(monitorData.cabcttmp_a);
+                $(".monitor-data-item-cabctalarm_a",$monitorItem).text(monitorData.cabctalarm_a);
+                $monitorItem.removeClass("template").appendTo("#monitorDataList");
+            });
+        }
     });
 }
